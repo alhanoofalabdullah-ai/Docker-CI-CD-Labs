ba7f75c4ac6d6c12e986fd98b3620c54bff840ca
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 16:06:58 +0300

    Create Main.java

---
+++ project-14-azure-cloud-infrastructure-platform/app/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Azure Infrastructure Deployment ");
+        System.out.println("====================================");
+
+        System.out.println("\nProvider Initialized");
+
+        System.out.println("\nResource Group Created");
+
+        System.out.println("\nVirtual Network Created");
+
+        System.out.println("\nSubnet Created");
+
+        System.out.println("\nNetwork Security Group Created");
+
+        System.out.println("\nVirtual Machine Created");
+
+        System.out.println("\nInfrastructure Ready");
+    }
+}
