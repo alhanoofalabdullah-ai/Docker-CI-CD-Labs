da26153f2f3274eb87adf2b5a0481a838ea1be9c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:44:19 +0300

    Create Main.java

---
+++ project-02-docker-compose-multi-container-application/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Docker Compose Application ");
+        System.out.println("======================================");
+
+        System.out.println("\nApplication Started");
+
+        System.out.println("\nDatabase Connection Established");
+
+        System.out.println("\nSystem Running Successfully");
+    }
+}
