1a4b783bdedb04e7b2fa6270e58753fb1032d0c8
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:32:38 +0300

    Create Main.java

---
+++ project-08-argocd-gitops-deployment-platform/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" ArgoCD GitOps Deployment ");
+        System.out.println("====================================");
+
+        System.out.println("\nRepository Connected");
+
+        System.out.println("\nApplication Registered");
+
+        System.out.println("\nCluster Synchronized");
+
+        System.out.println("\nDeployment Successful");
+
+        System.out.println("\nSystem Healthy");
+    }
+}
