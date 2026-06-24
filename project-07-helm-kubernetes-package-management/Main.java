9febdac7b6c3e4114c108b66cb7b45e154522214
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:23:32 +0300

    Create Main.java

---
+++ project-07-helm-kubernetes-package-management/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Helm Package Deployment ");
+        System.out.println("=====================================");
+
+        System.out.println("\nChart Installed Successfully");
+
+        System.out.println("\nRelease Created");
+
+        System.out.println("\nApplication Running");
+
+        System.out.println("\nUpgrade Available");
+
+        System.out.println("\nDeployment Healthy");
+    }
+}
