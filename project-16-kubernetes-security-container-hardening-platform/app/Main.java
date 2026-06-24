52a942953d353a9f0ffd854f0eef04bc0f7129de
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 16:26:24 +0300

    Create Main.java

---
+++ project-16-kubernetes-security-container-hardening-platform/app/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Kubernetes Security Platform ");
+        System.out.println("===================================");
+
+        System.out.println("\nCluster Security Validated");
+
+        System.out.println("\nNetwork Policies Applied");
+
+        System.out.println("\nRBAC Configured");
+
+        System.out.println("\nVulnerability Scan Passed");
+
+        System.out.println("\nSecure Deployment Completed");
+    }
+}
