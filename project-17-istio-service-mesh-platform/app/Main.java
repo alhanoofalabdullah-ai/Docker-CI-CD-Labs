fa975cca294ca52339cda5541e0be5c0e0c5454e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 16:38:22 +0300

    Create Main.java

---
+++ project-17-istio-service-mesh-platform/app/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Istio Service Mesh Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nIstio Installed");
+
+        System.out.println("\nGateway Created");
+
+        System.out.println("\nmTLS Enabled");
+
+        System.out.println("\nTraffic Routing Active");
+
+        System.out.println("\nObservability Enabled");
+    }
+}
