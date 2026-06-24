c1d914e4894d4bc1457dd435a4d89ee5f93d6d75
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 15:58:13 +0300

    Create Main.java

---
+++ project-13-aws-cloud-infrastructure-deployment-platform/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" AWS Infrastructure Deployment ");
+        System.out.println("====================================");
+
+        System.out.println("\nProvider Initialized");
+
+        System.out.println("\nVPC Created");
+
+        System.out.println("\nSubnet Created");
+
+        System.out.println("\nSecurity Group Created");
+
+        System.out.println("\nEC2 Instance Created");
+
+        System.out.println("\nInfrastructure Ready");
+    }
+}
