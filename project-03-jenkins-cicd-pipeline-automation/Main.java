744f89907354e3a007fb0e7b5b479be68ee3fc0e
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:51:23 +0300

    Create Main.java

---
+++ project-03-jenkins-cicd-pipeline-automation/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("===================================");
+        System.out.println(" Jenkins Pipeline Started ");
+        System.out.println("===================================");
+
+        System.out.println("\nApplication Build Successful");
+
+        System.out.println("\nTests Passed");
+
+        System.out.println("\nDeployment Successful");
+
+        System.out.println("\nPipeline Completed");
+    }
+}
