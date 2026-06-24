02c1c6f94310b571f1f48b069109294feed10b26
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:59:44 +0300

    Create Main.java

---
+++ project-10-elk-stack-log-management-platform/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("2026-06-24 10:00:01 INFO  Application started successfully");
+        System.out.println("2026-06-24 10:02:15 WARN  High memory usage detected");
+        System.out.println("2026-06-24 10:04:22 ERROR Database connection failed");
+        System.out.println("2026-06-24 10:05:45 INFO  User request processed");
+        System.out.println("2026-06-24 10:08:10 INFO  Application finished log generation");
+    }
+}
