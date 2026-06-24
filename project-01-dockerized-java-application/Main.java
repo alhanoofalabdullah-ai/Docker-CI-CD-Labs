43b8062cf2746b3ae00525a2e9c59c93c1c5e77c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:38:38 +0300

    Create Main.java

---
+++ project-01-dockerized-java-application/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println(" Docker CI/CD Labs ");
+        System.out.println("=================================");
+
+        System.out.println("\nApplication Started Successfully");
+
+        System.out.println("\nWelcome to Docker Project 01");
+
+        System.out.println("\nContainer Running Successfully");
+    }
+}
