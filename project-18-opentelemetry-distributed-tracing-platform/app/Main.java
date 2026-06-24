c4aa1c195aa59657ffcd5bfb96e14c493b4fef2c
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 16:50:19 +0300

    Create Main.java

---
+++ project-18-opentelemetry-distributed-tracing-platform/app/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" OpenTelemetry Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nCollector Started");
+
+        System.out.println("\nTrace Created");
+
+        System.out.println("\nSpan Generated");
+
+        System.out.println("\nTelemetry Exported");
+
+        System.out.println("\nJaeger Visualization Available");
+    }
+}
