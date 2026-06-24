b0aa448d799684623e9a671ad6470e351716fda6
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 16:17:12 +0300

    Create Main.java

---
+++ project-15-advanced-jenkins-enterprise-cicd-platform/app/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Advanced Jenkins Pipeline ");
+        System.out.println("====================================");
+
+        System.out.println("\nRepository Downloaded");
+
+        System.out.println("\nBuild Successful");
+
+        System.out.println("\nTests Passed");
+
+        System.out.println("\nQuality Gate Passed");
+
+        System.out.println("\nSecurity Scan Passed");
+
+        System.out.println("\nDocker Image Created");
+
+        System.out.println("\nDeployment Successful");
+
+        System.out.println("\nRelease Completed");
+    }
+}
