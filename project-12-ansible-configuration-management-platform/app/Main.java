e0b9c91721bb86e04980962938654d4335d5a088
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 15:47:55 +0300

    Create Main.java

---
+++ project-12-ansible-configuration-management-platform/app/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Ansible Configuration Platform ");
+        System.out.println("=====================================");
+
+        System.out.println("\nInventory Loaded");
+
+        System.out.println("\nPlaybook Executed");
+
+        System.out.println("\nPackages Installed");
+
+        System.out.println("\nServices Started");
+
+        System.out.println("\nConfiguration Applied Successfully");
+    }
+}
