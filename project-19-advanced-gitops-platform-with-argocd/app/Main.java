cba4fc9cab988dce65fdfe67f2de29af4c77868d
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 17:00:53 +0300

    Create Main.java

---
+++ project-19-advanced-gitops-platform-with-argocd/app/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println(" Advanced GitOps Platform ");
+        System.out.println("====================================");
+
+        System.out.println("\nRepository Connected");
+
+        System.out.println("\nApplication Registered");
+
+        System.out.println("\nCluster Synchronized");
+
+        System.out.println("\nDeployment Verified");
+
+        System.out.println("\nRollback Ready");
+
+        System.out.println("\nEnvironment Healthy");
+    }
+}
