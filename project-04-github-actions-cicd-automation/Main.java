6231e88721351ee617e453438e4cfdd2a086ff60
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 13:59:49 +0300

    Create Main.java

---
+++ project-04-github-actions-cicd-automation/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" GitHub Actions Pipeline ");
+        System.out.println("==================================");
+
+        System.out.println("\nRepository Downloaded");
+
+        System.out.println("\nBuild Successful");
+
+        System.out.println("\nTests Successful");
+
+        System.out.println("\nDeployment Completed");
+
+        System.out.println("\nPipeline Finished Successfully");
+    }
+}
