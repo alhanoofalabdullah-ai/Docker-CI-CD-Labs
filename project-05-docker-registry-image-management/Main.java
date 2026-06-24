41e7d320ddd4acddce8d24795a86f6fe8778eace
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:06:26 +0300

    Create Main.java

---
+++ project-05-docker-registry-image-management/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println(" Docker Registry Management ");
+        System.out.println("==================================");
+
+        System.out.println("\nApplication Built Successfully");
+
+        System.out.println("\nImage Tagged Successfully");
+
+        System.out.println("\nImage Published Successfully");
+
+        System.out.println("\nDeployment Ready");
+    }
+}
