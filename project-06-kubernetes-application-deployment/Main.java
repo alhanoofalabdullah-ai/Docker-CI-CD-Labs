a91c5738c15c07d7e8691add3328d948b55bf681
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:15:22 +0300

    Create Main.java

---
+++ project-06-kubernetes-application-deployment/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println(" Kubernetes Deployment ");
+        System.out.println("======================================");
+
+        System.out.println("\nNamespace Created");
+
+        System.out.println("\nDeployment Created");
+
+        System.out.println("\nPods Running");
+
+        System.out.println("\nService Available");
+
+        System.out.println("\nApplication Healthy");
+    }
+}
