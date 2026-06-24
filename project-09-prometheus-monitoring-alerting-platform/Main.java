0b3408bfefbdecebf73de78acd6bea5cb521c445
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 14:39:12 +0300

    Create Main.java

---
+++ project-09-prometheus-monitoring-alerting-platform/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Prometheus Monitoring Platform ");
+        System.out.println("=====================================");
+
+        System.out.println("\nMetrics Collection Started");
+
+        System.out.println("\nTargets Healthy");
+
+        System.out.println("\nCPU Usage Normal");
+
+        System.out.println("\nMemory Usage Normal");
+
+        System.out.println("\nNo Active Alerts");
+    }
+}
