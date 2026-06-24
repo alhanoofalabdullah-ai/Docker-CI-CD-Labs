27b0ae156cab7b9fa5cc26e15adda320ea4c952b
Alhanoof Alabdullah <alhanoofalabdullah@gmail.com>
2026-06-24 15:39:46 +0300

    Create Main.java

---
+++ project-11-terraform-infrastructure-as-code-lab/app/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("=====================================");
+        System.out.println(" Terraform Infrastructure as Code Lab ");
+        System.out.println("=====================================");
+
+        System.out.println("\nTerraform Initialized");
+        System.out.println("\nExecution Plan Created");
+        System.out.println("\nInfrastructure Provisioned");
+        System.out.println("\nOutputs Generated");
+        System.out.println("\nEnvironment Ready");
+    }
+}
